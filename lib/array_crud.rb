468bd0bda84dbba18714e3305260fa834dd5df70
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-08-06 02:26:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
  remove_element_from_end_of_array = ["arrays!", "array"]
- array_remove = remove_element_from_start_of_array.pop
+ array_remove = remove_element_from_end_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
